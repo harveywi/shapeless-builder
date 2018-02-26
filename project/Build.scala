043fd4b905e22daa659e55b572d1c1c944a63bd4
sh0hei <vividbit@gmail.com>
2018-02-26 22:30:48 +0900

    Update libraries

--- project/Build.scala
+++ project/Build.scala
@@ -47,8 +47,8 @@ object ShapelessBuilderBuild extends Build {
       ),
  
       libraryDependencies ++= Seq(
-        "com.chuusai" %% "shapeless" % "2.3.2" withSources(),
-        "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+        "com.chuusai" %% "shapeless" % "2.3.3" withSources(),
+        "org.scalatest" %% "scalatest" % "3.0.5-M1" % "test"
       )
     )
 }
